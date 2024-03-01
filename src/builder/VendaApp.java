dca2a25f9aa7713f673cbee3bd06d023864414e4
[name] <[email address]>
2024-03-01 16:00:53 -0300

    implementa exemplo padrao de projeto builder

---
+++ src/builder/VendaApp.java
@@ -0,0 +1,17 @@
+package builder;
+
+import java.math.BigDecimal;
+
+public class VendaApp {
+    public static void main(String[] args) {
+
+        Pedido pedido = new PedidoBuilder()
+                .setNumero("1234567891")
+                .setCliente(100, "Ricardo",  "99988-0000")
+                .setVendedor(1, "Mario")
+                .setProduto("Notebook",1, 8000.00)
+                .setProduto("Iphone", 2, 5000.00)
+                .builder();
+        System.out.println(pedido);
+    }
+}
